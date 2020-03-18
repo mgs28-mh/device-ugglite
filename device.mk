d8471dc0d3bd29c3b9ee577c4ffc757eec398214
TheStrix <parthbhatia98@gmail.com>
2020-03-19 02:38:50 +0530

    ugglite: Use new QTI components from qcom/common
    Change-Id: Idab3a2cf06813bfa8e38811faeb1266c4d3b7434

--- device.mk
+++ device.mk
@@ -402,6 +402,12 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-full \
     libxml2
 
+# QTI common
+TARGET_COMMON_QTI_COMPONENTS := \
+    av \
+    bt \
+    perf
+
 # RCS
 PRODUCT_PACKAGES += \
     PresencePolling \
